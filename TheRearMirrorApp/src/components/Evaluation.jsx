7a50bb532dd901b0ad8d3e2640e9d6d4bbb30f21
LucaDiGiacomo <lucamaria.digiacomo@gmail.com>
2024-01-17 17:45:11 +0100

    LDG - added lesson element

--- TheRearMirrorApp/src/components/Evaluation.jsx
+++ TheRearMirrorApp/src/components/Evaluation.jsx
@@ -10,6 +10,45 @@ import RangeSlider from 'react-bootstrap-range-slider';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
+function LessonElement(lesson) {
+  const bToEvaluate = lesson.evaluated;
+  if (bToEvaluate) {
+    <div className="label">
+      <div className="text-wrapper">LESSON lesson.date</div>
+      <div className="lesson-scenarios">
+        lesson.scenario1
+        <br />
+        lesson.scenario2
+        <br />
+        lesson.scenario3
+      </div>
+      <div className="stars">
+        {/* <todo: eval stars> */}
+        <div className="overlap-group">
+          <img className="review" alt="Review" src="review.svg" />
+          <img className="img" alt="Review" src="image.svg" />
+          <img className="review-2" alt="Review" src="review-2.svg" />
+          <img className="review-3" alt="Review" src="review-3.svg" />
+          <img className="review-4" alt="Review" src="review-4.svg" />
+        </div>
+      </div>
+    </div>
+  }
+  else {
+    <div className="label">
+      <div className="text-wrapper">LESSON lesson.date</div>
+      <div className="lesson-scenarios">
+        lesson.scenario1
+        <br />
+        lesson.scenario2
+        <br />
+        lesson.scenario3
+      </div>
+      <Button variant="outlined">Evaluate</Button>
+    </div>
+  }
+}
+
 const Evaluation = () => {
   const [isChecked, setIsChecked] = useState(false);
 
@@ -18,7 +57,9 @@ const Evaluation = () => {
   };
   const [date, setDate] = useState(new Date());
 
-
+  var React = require('react');
+  var ReactDOM = require('react-dom');
+  var ScrollArea = require('react-scrollbar');
   return (
 
     <div>
@@ -27,48 +68,39 @@ const Evaluation = () => {
       </header>
       <Container>
         <Form>
-          <div>
-
-            <Row>
-              <Col>
-                <Form.Group controlId="datePicker">
-                  <Form.Label className='fw-light'>Date</Form.Label>
-                </Form.Group>
-                <Form.Group controlId="duedate">
-                  <Form.Control
-                    type="date"
-                    name="duedate"
-                    placeholder="Date"
-                    value={date}
-                    onChange={(e) => setDate(e.target.value)}
-                  />
-                </Form.Group>
-              </Col>
-              <Col>
-                <Form.Group controlId="validateChk">
-                  <Form.Label className='fw-light'>Validated</Form.Label>
-                </Form.Group>
-                <Form.Group controlId="formCheckbox">
-                  <Form.Check
-                    type="checkbox"
-                    label=""
-                    checked={isChecked}
-                    onChange={handleCheckboxChange}
-                  />
-                </Form.Group>
-              </Col>
 
 
-            </Row>
-          </div>
+          <Row>
+            <Col>
+              <Form.Group controlId="datePicker">
+                <Form.Label className='fw-light'>Date</Form.Label>
+              </Form.Group>
+              <Form.Group controlId="duedate">
+                <Form.Control
+                  type="date"
+                  name="duedate"
+                  placeholder="Date"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="validateChk">
+                <Form.Label className='fw-light'>Validated</Form.Label>
+              </Form.Group>
+              <Form.Group controlId="formCheckbox">
+                <Form.Check
+                  type="checkbox"
+                  label=""
+                  checked={isChecked}
+                  onChange={handleCheckboxChange}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
         </Form>
-
-
       </Container>
-
-
-
-
 
       <footer className="myNavbar">
         <MyNavbar></MyNavbar>
