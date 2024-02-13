7ba62d1fe5761e5f5631f018f942a31ea28b4c95
serenaaprano13 / serena aprano <serenaaprano3@gmail.com>
2024-02-13 11:09:56 +0100

    done front end on planning lesson

--- TheRearMirrorApp/src/components/PlanningForm.jsx
+++ TheRearMirrorApp/src/components/PlanningForm.jsx
@@ -9,12 +9,32 @@ import {useState} from 'react';
 import RangeSlider from 'react-bootstrap-range-slider';
 import Col from 'react-bootstrap/Col';
 import { Multiselect } from 'multiselect-react-dropdown';
+import { MDBRange } from 'mdb-react-ui-kit';
+import Button from 'react-bootstrap/Button';
+import {useNavigate} from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
 
 const PlanningForm = () => {
 
   const [ distance, setDistance ] = useState(1);
   const [ finalDistance, setFinalDistance ] = useState(distance);
-  
+  const [showDiscardModal, setShowDiscardModal]=useState(false);
+
+  const navigate = useNavigate();
+  const handleDiscard = (event) => {
+    event.preventDefault();
+    setShowDiscardModal(true);
+  }
+  const confirmDiscard = () => {
+    setShowDiscardModal(false);
+    navigate('/');
+  };
+  const cancelDiscard = () => {
+    setShowDiscardModal(false);
+  };
+
+
+
 
 
   const option = [
@@ -43,27 +63,28 @@ const PlanningForm = () => {
   return (
 
     <div>
-      <header>
-        <Title titolo="Planning"></Title>
-      </header>
-    
+    <Title titolo="Planning"></Title>
     <Form className="planning">
-      <Form.Group controlId="plannedDistance">
-        <Form.Label className='fw-light'>Planned Distance</Form.Label>
-        
-          <RangeSlider className="range"
-            value={distance}
-            onChange={e => setDistance(e.target.value)}
-            variant='success'
-            tooltip='on'
-            min={0.5}
-            max={10}
-            step={0.5}
-          />
+      <Form.Group className="form-group" controlId="plannedDistance">
+        <Form.Label className='custom-label'>Planned Distance</Form.Label>{" "}
       </Form.Group>
 
-      <Form.Group controlId="lastestMistakes">
-        <Form.Label className='fw-light'>Recent Mistakes</Form.Label>
+
+      <Form.Group>
+
+        {/*MD RANGE SLIDER  */}
+        <MDBRange
+          defaultValue={2.5}
+          min='0'
+          max='10'
+          step='0.5'
+          className="range"
+        />
+       
+      </Form.Group>
+
+      <Form.Group className="form-group" controlId="lastestMistakes">
+        <Form.Label className='custom-label'>Recent Mistakes</Form.Label>
     
         <Multiselect
           className="planningMultiSelect"
@@ -72,7 +93,7 @@ const PlanningForm = () => {
           //onSelect={this.onSelect} // Function will trigger on select event
           //onRemove={this.onRemove} // Function will trigger on remove event
           displayValue="name" // Property name to display in the dropdown optionsù
-          placeholder='Type and search mistakes'
+          placeholder='Type & search mistakes'
           emptyRecordMsg='Driving scenario not found'
           closeIcon='cancel'
           closeOnSelect={false}
@@ -85,22 +106,48 @@ const PlanningForm = () => {
         />
       </Form.Group>
 
-      <Form.Group controlId="lastestMistakes">
-        <Form.Label className='fw-light'>Untested</Form.Label>
+      <Form.Group className="form-group" controlId="lastestMistakes">
+        <Form.Label className='custom-label'>Untested Scenarios</Form.Label>
         <Multiselect
+          className="planningMultiSelect"
           options={option} // Options to display in the dropdown
          // selectedValues={selectedValue} // Preselected value to persist in dropdown
           //onSelect={this.onSelect} // Function will trigger on select event
           //onRemove={this.onRemove} // Function will trigger on remove event
           displayValue="name" // Property name to display in the dropdown options
+          placeholder='Type & search scenarios'
+          emptyRecordMsg='Driving scenario not found'
+          closeIcon='cancel'
+          closeOnSelect={false}
+          avoidHighlightFirstOption={true}
+          hidePlaceholder={true}
+          showArrow={true} 
+          keepSearchTerm={true}
+          
         />
       </Form.Group>
 
+      <Form.Group className="d-flex justify-content-center ">
+        <button className="discard-btn" onClick={(event)=>handleDiscard(event)}>DISCARD PLAN</button>{" "}
+        <button className="save-btn">SAVE PLAN</button>
+      </Form.Group>
+
+
+    </Form>
+
+  <Modal show={showDiscardModal} onHide={cancelDiscard}>
+    <Modal.Header closeButton>
+      <Modal.Title>Confirm Discard</Modal.Title>
+    </Modal.Header>
+  <Modal.Body>Are you sure you want to discard your changes and leave to Homepage?</Modal.Body>
+  <Modal.Footer>
+    <Button variant="secondary" onClick={cancelDiscard}>Cancel</Button>
+    <Button variant="primary" onClick={confirmDiscard}>Discard planning</Button>
+  </Modal.Footer>
+</Modal>
 
 
 
-
-  </Form>
 
 
  
