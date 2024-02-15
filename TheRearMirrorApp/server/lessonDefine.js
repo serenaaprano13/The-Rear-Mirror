76ac643ff513b68cb12c0d7fddef14f9d91fb91e
LucaDiGiacomo <lucamaria.digiacomo@gmail.com>
2024-02-15 18:07:02 +0100

    added some backend for LESSONS

--- TheRearMirrorApp/server/lessonDefine.js
+++ TheRearMirrorApp/server/lessonDefine.js
@@ -1,12 +1,12 @@
 
 
-function Lesson(date, scenario1, scenario2, scenario3, grade, bEvaluated, distance) {
+function Lesson(date, scenario1, scenario2, scenario3, grade, rifEvaluation, distance) {
     this.date = date;
     this.scenario1 = scenario1;
     this.scenario2 = scenario2;
     this.scenario3 = scenario3;
     this.grade = grade;
-    this.evaluated = bEvaluated;
+    this.evaluated = rifEvaluation;
     this.distance = distance;
 }
 
--- TheRearMirrorApp/server/plannings-dao.js
+++ TheRearMirrorApp/server/plannings-dao.js
@@ -3,6 +3,7 @@
 const sqlite = require('sqlite3');
 const dayjs = require('dayjs');
 const { Planning} = require('./planning.js');
+const { Lesson } = require('./lessonDefine.js');
 
 const db = new sqlite.Database('theRearMirrorDB.db', (err) => {
     if (err) throw err;
@@ -103,20 +104,40 @@ exports.getLessons=(validated)=>{
     return new Promise( (resolve,reject)=>{
         if(validated)
         {
-            const sql = ` SELECT * FROM LESSONS WHERE rif_evaluation<>-1 ORDER BY lessonDate ASC ;`;
+            const sql = ` SELECT lesson_id,rif_evaluation,lessonDate,grade
+                 FROM LESSONS l,EVALUATIONS e WHERE rif_evaluation<>-1 ORDER BY lessonDate ASC ;`;
         }
         else
         {
 
-            const sql = ` SELECT * FROM LESSONS  ORDER BY lessonDate ASC  ;`;
+            const sql = ` SELECT L.lesson_id, L.lessonDate,L.rif_evaluation,
+            MAX(CASE WHEN ROWNUM = 1 THEN DS.scenario_name END) AS scenario_name_1,
+            MAX(CASE WHEN ROWNUM = 2 THEN DS.scenario_name END) AS scenario_name_2,
+            MAX(CASE WHEN ROWNUM = 3 THEN DS.scenario_name END) AS scenario_name_3,
+            E.grade
+        FROM
+            LESSONS L, EVALUATIONS E
+        JOIN
+            (SELECT
+                    LS.rif_lesson,
+                    DS.scenario_name,
+                    ROW_NUMBER() OVER (PARTITION BY LS.rif_lesson ORDER BY LS.idRow) AS ROWNUM
+                FROM
+                    LINKLessonScenario LS
+                JOIN
+                    DRIVINGSCENARIOS DS ON LS.scenario_id = DS.scenario_id
+            ) DS ON L.lesson_id = DS.rif_lesson
+        GROUP BY
+            L.lesson_id, L.lessonDate;`;
         }
         db.all(sql,[],(err,rows)=>{
             if(err){
                 reject(err);
                 return;
             }
-            const plannings = rows.map((p) => new Planning(p.planning_id, p.creation_date, p.distance));
-            resolve(plannings[0]);
+            const lessons = rows.map((l) => new Lesson(l.lessonDate,l.scenario_name_1,l.scenario_name_2,l.scenario_name_3,l.grade,l.rif_evaluation,0));
+            //date, scenario1, scenario2, scenario3, grade, rifEvaluation, distance)
+            resolve(lessons[0]);
         });
     });
 };
