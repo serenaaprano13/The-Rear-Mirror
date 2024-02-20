b703721e88974b2493bef9162947a4457f8c5708
LucaDiGiacomo <lucamaria.digiacomo@gmail.com>
2024-02-20 15:53:20 +0100

    merging db

--- TheRearMirrorApp/server/lessonDefine.js
+++ TheRearMirrorApp/server/lessonDefine.js
@@ -1,7 +1,9 @@
 'use strict' ;
 
 // LESSONS(lesson_id,rif_evaluation,lessonDate,scenario_1,scenario_2,scenario_3,grade,distance,to_evaluate)
-function Lesson(date, scenario1, scenario2, scenario3, grade, rifEvaluation, distance, to_evaluate) {
+
+function Lesson(date, scenario1, scenario2, scenario3, grade, rifEvaluation, distance, to_evaluate,
+    route_1,route_2,route_3,mistake_1,mistake_2,mistake_3) {
     this.date = date;
     this.scenario1 = scenario1;
     this.scenario2 = scenario2;
@@ -10,6 +12,12 @@ function Lesson(date, scenario1, scenario2, scenario3, grade, rifEvaluation, dis
     this.evaluated = rifEvaluation;
     this.distance = distance;
     this.to_evaluate = to_evaluate;
+    this.route_1=route_1;
+    this.route_2=route_2;
+    this.route_3=route_3;
+    this.mistake_1=mistake_1;
+    this.mistake_2=mistake_2;
+    this.mistake_3=mistake_3;
 }
 
 //ROUTES(route_id,rif_lesson,routeName,distanceInKm)
