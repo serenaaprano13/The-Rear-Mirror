e6074d24c469dfa09ad7e21b3c5a3d735f4b9c21
Femaf <maffeofederico52@gmail.com>
2024-02-26 14:02:55 +0100

    fix keyword

--- TheRearMirrorApp/src/components/LookBack.jsx
+++ TheRearMirrorApp/src/components/LookBack.jsx
@@ -82,8 +82,11 @@ function LookBack() {
   };
 
   const showAllLessons = () => {
+    fetchAllLessons().then(() => {
     setFilteredLessons(lessons);
-  };
+    }
+    );
+  }
 
 
   const handleSearchChange = (event) => {
@@ -97,7 +100,7 @@ function LookBack() {
         lesson.scenario3.toLowerCase().includes(keywordLower);
     });
 
-    setLessons(filteredLessons);
+    setFilteredLessons(filteredLessons);
   }
 
   const handleAskToEvaluateClick = (lesson) => {
@@ -157,7 +160,7 @@ function LookBack() {
       </Container>*/}
       <Container fluid className="date-keyword-bar">
         <Row>
-          <Col>
+          <Col xs={6}>
             <InputGroup>
               <div style={{ display: 'flex', alignItems: 'center' }}>
                 <InputGroup.Text>
@@ -165,14 +168,22 @@ function LookBack() {
                     <path fillRule="evenodd" clipRule="evenodd" d="M8.36251 0C8.9148 0 9.36251 0.447715 9.36251 1V2.84772H19.4963V1C19.4963 0.447716 19.944 0 20.4963 0C21.0486 0 21.4963 0.447716 21.4963 1V2.84772H21.8588C25.7248 2.84772 28.8588 5.98173 28.8588 9.84772V21.1318C28.8588 24.9978 25.7248 28.1318 21.8588 28.1318H7C3.134 28.1318 0 24.9978 0 21.1318V9.84772C0 5.98172 3.13401 2.84772 7 2.84772H7.36251V1C7.36251 0.447715 7.81023 0 8.36251 0ZM19.4963 4.84772V6.74655C19.4963 7.29883 19.944 7.74655 20.4963 7.74655C21.0486 7.74655 21.4963 7.29883 21.4963 6.74655V4.84772H21.8588C24.6202 4.84772 26.8588 7.0863 26.8588 9.84772V21.1318C26.8588 23.8933 24.6202 26.1318 21.8588 26.1318H7C4.23857 26.1318 2 23.8932 2 21.1318V9.84772C2 7.0863 4.23858 4.84772 7 4.84772H7.36251V6.74655C7.36251 7.29883 7.81023 7.74655 8.36251 7.74655C8.9148 7.74655 9.36251 7.29883 9.36251 6.74655V4.84772H19.4963ZM4.14513 12.0514C4.14513 10.9469 5.04056 10.0514 6.14513 10.0514H10.3346V10.0514H11.3346H17.5242H18.5242H22.7137C23.8183 10.0514 24.7137 10.9468 24.7137 12.0514V16.2409V17.2409V18.2409V22.4305C24.7137 23.535 23.8183 24.4305 22.7137 24.4305H18.5242H17.5242H11.3346H10.3346H6.14512C5.04054 24.4305 4.14512 23.535 4.14512 22.4305V17.2409V16.2409L4.14513 12.0514ZM16.5242 22.4305V18.2409L12.3346 18.2409V22.4305L16.5242 22.4305ZM18.5242 16.2409H22.7137L22.7137 12.0514L18.5242 12.0514L18.5242 16.2409ZM18.5242 22.4305L22.7137 22.4305V18.2409H18.5242L18.5242 22.4305ZM12.3347 12.0514H16.5242V16.2409H12.3347V12.0514V12.0514ZM10.3346 18.241L10.3346 22.4305L6.14512 22.4305V18.241H10.3346ZM10.3346 16.2409H6.14513V12.0514L10.3346 12.0514L10.3346 16.2409Z" fill="black" />
                   </svg>
                 </InputGroup.Text>
-                <ReactDatePicker
-                  selected={startDate}
-                  onChange={handleDateChange}
-                  renderDayContents={renderDayContents}
-                />              </div>
+
+
+                <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+                  <div style={{ flex: '0 0 50%' }}>
+                    <ReactDatePicker
+                      selected={startDate}
+                      onChange={handleDateChange}
+                      renderDayContents={renderDayContents}
+                    />
+                  </div>
+                </div>
+              </div>
+
             </InputGroup>
           </Col>
-          <Col>
+          <Col xs={6}>
             <InputGroup>
               <FormControl type="text" placeholder="Keyword" aria-label="keyword" value={searchKeyword} onChange={handleSearchChange} />
               <InputGroup.Text>
